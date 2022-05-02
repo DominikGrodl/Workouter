67e6cc4dc068b0433783846a58709bc4044fe5a9
DominikGrodl <dominik.grodl@gmail.com>
2022-05-02 11:46:36 +0200

    Initial Commit

---
+++ Workouter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Workouter
+//
+//  Created by Dominik Grodl on 02.05.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Workouter/WorkouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkouterApp.swift
+//  Workouter
+//
+//  Created by Dominik Grodl on 02.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
